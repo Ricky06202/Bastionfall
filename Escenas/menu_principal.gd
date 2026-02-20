e96b021fdd9f2d757dc3fa53ccbb6624d425c072
Ricardo A. Sanjur G. <91886986+Ricky06202@users.noreply.github.com>
2026-02-20 10:31:48 -0500

    hicimos el menu principal

---
+++ Escenas/menu_principal.gd
@@ -0,0 +1,11 @@
+extends Control
+
+func _on_salir_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_opciones_pressed() -> void:
+	pass # Replace with function body.
+
+func _on_jugar_pressed() -> void:
+	get_tree().change_scene_to_file("res://Escenas/nivel.tscn")
